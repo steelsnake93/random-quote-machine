5c91a82a41a2dfbe299e6787e02cdd6ac1e6dc61
steelsnake93 <nikolanikotasev@protonmail.com>
2023-02-24 16:59:57 +0100

    Added functionality to fetch random quotes and display them in the UI. Also added the necessary dependencies and configurations for running tests using the FreeCodeCamp Test Suite

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function App() {
+  const [quoteData, setQuoteData] = useState({});
+
+const getRandomQuote = async () => {
+  try {
+    const response = await axios.get('https://api.quotable.io/random');
+    const { content, author } = response.data;
+    return { content, author };
+    } catch (error) {
+      console.error(error);
+      return { content: 'Sorry, something went wrong', author: 'Unknown' };
+    }
+  }
+  
+  const handleClick = async () => {
+    const { content, author } = await getRandomQuote();
+    setQuoteData({ content, author });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{quoteData.content}</p>
+        <p>- {quoteData.author}</p>
+        <button onClick={handleClick}>Get Random Quote</button>
       </header>
     </div>
   );
